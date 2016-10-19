9728d6bed80e50bcd67e826d810d00ae05e1cc57
Barada <telnowtema@mail.ru>
2016-10-19 17:27:48 +0300

    New_File

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tz::Application.config.secret_key_base = '0a82c79a2ad5633136f6ee86aa1b3a7e3bc08157023f508181f40e73c1ee78122c4861bfef6e96b90a25a39fc8c37fd5b55873e8175496714c4bc349e50a9f83'
